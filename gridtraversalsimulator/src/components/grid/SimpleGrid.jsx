2d7ca324a5b4c117a60ab6676f9e9bec6c7838cc
Eyüp Can Yönter <oniraca.om00@gmail.com>
2023-08-03 15:19:22 +0300

    03.08.01

--- gridtraversalsimulator/src/components/grid/SimpleGrid.jsx
+++ gridtraversalsimulator/src/components/grid/SimpleGrid.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export function SimpleGrid({ rows, columns, selectingMode }) {
-    const [grids, setGrids] = useState([]);
+    const createInitialGrids = (rows, columns) => {
+        const newArray = [];
+        for (let i = 0; i < rows; i++) {
+            const row = [];
+            for (let j = 0; j < columns; j++) {
+                row.push(0); // Initialize with some default value
+            }
+            newArray.push(row);
+        }
+        return newArray;
+    };
 
+    const [grids, setGrids] = useState(createInitialGrids);
+
+    useEffect(() => {
+        setGrids(createInitialGrids(rows, columns));
+    }, [rows, columns]);
     const gridContainerStyle = {
         display: 'flex',
         flexWrap: 'wrap',
@@ -13,22 +27,29 @@ export function SimpleGrid({ rows, columns, selectingMode }) {
 
     const gridItemStyle = {
         flexBasis: `calc(100% / ${columns} - 10px)`, // Adjust the width of grid items based on the number of columns
+        width: '5px',
         height: '10px', // Adjust the height of grid items as needed
         backgroundColor: 'white', // Change the background color of grid items
         border: '1px solid black', // Add a border around grid items
+        marginBottom: '3px',
     };
-
-    // Generate grid content dynamically using a loop
-    const gridContent = [];
-    for (let i = 0; i < rows; i++) {
-        const row = []; // Initialize each row as an empty array
-        for (let j = 0; j < columns; j++) {
-            row.push(<div id={`${i}-${j}`} style={gridItemStyle}></div>);
-        }
-        gridContent.push(row); // Push the row (array of divs) to the 2D array
-    }
-    console.log(gridContent)
-    //setGrids(gridContent);
-
-    return <div style={gridContainerStyle}>{gridContent}</div>;
+    return (
+        <div style={gridContainerStyle}>
+            {grids.map((row, i) => (
+                <div key={i}>
+                    {row.map((cell, j) => (
+                        <div
+                            key={`${i}-${j}`}
+                            style={gridItemStyle}
+                            onClick={() => {
+                                const updatedGrids = [...grids];
+                                updatedGrids[i][j] = (updatedGrids[i][j] + 1) % 2;
+                                setGrids(updatedGrids);
+                            }}
+                        ></div>
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
 }
