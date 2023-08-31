81f37b7b5d269297e99f5f09fe9e94160ed40329
Eyüp Can Yönter <oniraca.om00@gmail.com>
2023-08-31 11:42:29 +0300

    ll

---
+++ native/scripts/random.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.just-wrapper').on('click', function() {
+        $('.material-symbols-outlined').toggleClass('flipped');
+        $('.settings-bar-wrapper').toggle(300);
+    });
+
+    $('.settings-toggler-wrapper').hover(
+        function() {
+            $(this).css({
+                'outline': '1px solid #ffffff',
+                'border-radius': '1vh'
+            }); // Add outline when hovered
+        },
+        function() {
+            $(this).css('outline', 'none'); // Remove outline when not hovered
+        }
+    );
+})
