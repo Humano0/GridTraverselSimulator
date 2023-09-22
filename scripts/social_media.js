88a7e803f05b3620fc0789ea00e3a5cbfddb48f7
Eyüp Can Yönter <oniraca.om00@gmail.com>
2023-09-22 18:24:14 +0300

    added social media links

---
+++ scripts/social_media.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+    $('.github-img').click(() => {
+        window.open('https://github.com', '_blank');
+    });
+
+    $('.linkedin-img').click(() => {
+        window.open('https://linkedin.com', '_blank');
+    });
+});
