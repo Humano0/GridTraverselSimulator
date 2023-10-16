671f7a637a9853cb123a0dcb3334e6498247c9f7
Eyüp Can Yönter <oniraca.om00@gmail.com>
2023-10-16 18:09:13 +0300

    deleted dfs from earth

--- scripts/form_actions.js
+++ scripts/form_actions.js
@@ -321,80 +321,14 @@ async function bfs (grid, startnode, endnode) {
 
 
 /*
-    *******DFS*******
-    *******DFS*******
-    *******DFS*******
-    *******DFS*******
-    *******DFS*******
+    *******AStar*******
+    *******AStar*******
+    *******AStar*******
+    *******AStar*******
+    *******AStar*******
 */
-// DFS broken
-async function solveDFS(startnode, endnode, adjacencyList, visited, rownumber, columnnumber, ) {
-    let stack = [];
-    let breaker = false;
-    let id = splitID(startnode);
-    stack.push(startnode);
-    visited[id[0]][id[1]] = true;
-    let prev = createPreviousArray(rownumber, columnnumber);
 
-    while(stack.length) {
-        let node = stack.pop();
-        const neighbors = adjacencyList[node];
-        if(neighbors != null) {
-            for(let next of neighbors) {
-                console.log(next);
-                let nextid = splitID(next);
-                let visitedElem = document.querySelector(`#${next}`);
-                if (visitedElem.id !== startnode && visitedElem.id !== endnode) {
-                    visitedElem.classList.add('visited-node');
-                    // Delay for a smoother effect
-                    await delay(10);
-                    // Increment the opacity (e.g., by 0.1 each time)
-                    visitedElem.style.opacity = parseFloat(visitedElem.style.opacity) + 0.1;
-                }
-                if(!visited[nextid[0]][nextid[1]]) {
-                    stack.push(next);
-                    visited[nextid[0]][nextid[1]] = true;
-                    prev[nextid[0]][nextid[1]] = node;
-                }
-                if(next === endnode) {
-                    prev[nextid[0]][nextid[1]] = node;
-                    breaker = true;
-                    break;
-                }
-            }
-        }
-        if(breaker) {
-            break;
-        }
-    }
-    return prev;
-}
-async function reconstructPathDFS(startnode, endnode, prev, lightElementCallback) {
-    let path = [];
-    let at = splitID(endnode);
 
-    for (let pos = endnode; pos != null; pos = prev[at[0]][at[1]]) {
-        at = splitID(pos);
-        path.push(pos);
-    }
-    path.reverse();
-
-    if (path[0] == startnode) {
-        await lightThePath(path);
-    } else {
-        console.log('no path found');
-    }
-}
-async function dfs(grid, startnode, endnode) {
-    const rownumber = grid.length;
-    const columnnumber = grid[0].length;
-    const adjacencyList = createAdjacencyList(grid);
-    let visited = createVisitedArray(rownumber, columnnumber);
-
-    const prev = await solveDFS(startnode, endnode, adjacencyList, visited, rownumber, columnnumber);
-    console.log(prev);
-    await reconstructPathDFS(startnode, endnode, prev);
-}
 
 function clear() {
     const gridElements = document.querySelectorAll('.grid-cell');
